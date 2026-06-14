74e1ec7454e842c159f610efe8b76bf9a9a1aa33
HieuITMHG <danghuuhieu038@gmail.com>
2026-06-14 21:44:30 +0700

    fix qdrant

--- core/qdrant.py
+++ core/qdrant.py
@@ -2,9 +2,7 @@ from qdrant_client import QdrantClient
 from qdrant_client.models import PointStruct, VectorParams, Distance
 from core.config import settings
 
-client = QdrantClient(url=settings.qdrant_endpoint, api_key=settings.qdrant_key)
-
-test_client = QdrantClient(url="http://localhost:6333/", api_key=settings.qdrant_key)
+client = QdrantClient(url=settings.qdrant_endpoint)
 
 if not client.collection_exists(settings.upload_collection):
     client.create_collection(
